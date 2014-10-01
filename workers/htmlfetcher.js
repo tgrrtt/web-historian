b757fc60ec3f721d156ba6ba164863f52dc38c07
tgrrtt <jtgarrett123@gmail.com>
2014-10-01 16:30:03 -0700

    htmlfetcher.js fetches html from sites.txt

--- workers/htmlfetcher.js
+++ workers/htmlfetcher.js
@@ -1,4 +1,21 @@
 // eventually, you'll have some code here that uses the code in `archive-helpers.js`
 // to actually download the urls you want to download.
 //
-// require helpers/archive-helpers
+// fetch bot
+var path = require('path');
+
+var helper = require('../helpers/archive-helpers.js');
+//console.log(helper.paths.list);
+helper.readListOfUrls(helper.paths.list, function(siteArr){
+  // generate path based on urls
+  for (var i = 0; i < siteArr.length;i++) {
+    var siteUrl = siteArr[i];
+    var filePath = path.join(helper.paths.archivedSites, siteUrl);
+    // check if each url is archived
+    helper.isURLArchived(filePath,function(){
+      // if not, downloadUrls();
+      helper.downloadUrls(siteUrl,filePath);
+    });
+  }
+
+});
