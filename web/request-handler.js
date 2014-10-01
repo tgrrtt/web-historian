4db6af69352d6a29560ecfb5b3ad393870bd067b
tgrrtt <jtgarrett123@gmail.com>
2014-10-01 15:05:10 -0700

    Added notes

--- web/request-handler.js
+++ web/request-handler.js
@@ -3,5 +3,14 @@ var archive = require('../helpers/archive-helpers');
 // require more modules/folders here!
 
 exports.handleRequest = function (req, res) {
+  // Path: '/'
+  //   GET: server up index.html
+  //   POST: (fn NAME): boolean lookup in sites.txt
+  //         if in archives.
+  //            respond html archived
+  //         else
+  //            respond loading.
+  // Path: '/' + "SITEURL"
+  //    GET: (fn NAME)
   res.end(archive.paths.list);
 };
